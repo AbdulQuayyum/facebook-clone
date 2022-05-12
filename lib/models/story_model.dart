31656fd07b39c1b6d3d2da4876fac3176f8c1cd6
Abdul-Quayyum <76252147+AbdulQuayyum@users.noreply.github.com>
2022-05-12 12:03:51 +0100

    Add files via upload

--- lib/models/story_model.dart
+++ lib/models/story_model.dart
@@ -1,5 +1,5 @@
 import 'package:meta/meta.dart';
-import 'package:facebook_clone/models/models.dart';
+import 'package:flutter_facebook_responsive_ui/models/models.dart';
 
 class Story {
   final User user;
